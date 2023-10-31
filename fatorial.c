74094e54e81c8758d23b95ba05b5975ce9452f77
Késia Silva <kesiasilvab6@gmail.com>
2023-10-31 09:09:48 -0300

    Update fatorial.c

--- fatorial.c
+++ fatorial.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-    //fatorial de um numero
-    int num, fatorial = 1, i= 1;
-
+int main()
+{
+    // fatorial de um numero
+    int num, fatorial = 1, i = 1;
     printf("digite um numero para calcular seu fatorial: \n ");
     scanf("%i", &num);
-    
-    for(num; num<=0; num++){
-    
-         if(num < 0){
+
+    for (num; num <= 0; num)
+    {
+
+        if (num < 0)
+        {
             printf("numero invalido\n");
-    
-         }else if(num == 0){
+            break;
+        }
+        else if (num == 0)
+        {
             printf("fatorial = 1");
             break;
         }
     }
-    for(i; i<=num; i++){
-      fatorial *= i;
+    for (i; i <= num; i++)
+    {
+        fatorial *= i;
     }
+   if (num > 0)
+   {
      printf("fatorial = %i\n", fatorial);
+   }
+   
 }
